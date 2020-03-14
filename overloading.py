3fd48aee6ccccee2620fa4c149e2acd55db57447
PrabhatM123 <60963351+PrabhatM123@users.noreply.github.com>
2020-03-14 11:55:51 +0530

    Update overloading.py

--- overloading.py
+++ overloading.py
@@ -9,7 +9,7 @@ class derived_sum(overloading_sum):
     
     def area(self,a,b,c):
         super(derived_sum,self).area(10,20)
-        print('volume of box =',a*b*c)
+        print('volume of box =',a*b*c)        # print the volume of box
 
 derived_sum_obj=derived_sum()
 derived_sum_obj.area(10,20,30)
